82a64a51150adbc65b296295326b072416f8f27c
yoshimi-I <iwatsuki.443@outlook.com>
2023-04-07 18:55:12 +0900

    add: graphQLのセットアップ完了

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 })
 export class AppModule {}
